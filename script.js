e76395cbf5e5e8f016c5a69e9eae64ca71ba86f7
Sanjana Deshmukh <92514417+Sanjana-Deshmukh06@users.noreply.github.com>
2024-01-29 18:11:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "PHP Developer", "Software Developer", "Fresher", "Freelancer"],
+        strings: ["Web Developer", "Frontend Developer", "Software Developer", "Fresher", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "PHP Developer", "Software Developer", "Fresher", "Freelancer"],
+        strings: ["Web Developer", "Frontend Developer", "Software Developer", "Fresher", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
